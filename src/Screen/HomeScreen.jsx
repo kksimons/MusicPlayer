ece2622318daf220c5deb1c85f57f19f58e1f687
kksimons <kksimons@gmail.com>
2024-06-16 21:20:42 -0600

    added fonts

--- src/Screen/HomeScreen.jsx
+++ src/Screen/HomeScreen.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Header from '../components/Header';
 import { colors } from '../constants/colors';
-import { iconSizes, spacing } from '../constants/dimensions';
+import { fontSize, iconSizes, spacing } from '../constants/dimensions';
+import { fontFamilies } from '../constants/fonts';
 
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <Header />
+            <Text style={styles.headingText}>Recommended for you</Text>
         </View>
     );
 }
@@ -17,6 +19,11 @@ const styles = StyleSheet.create({
         backgroundColor: colors.background,
         flex: 1,
     },
+    headingText: {
+        fontSize: fontSize.xl,
+        color: colors.textPrimary,
+        fontFamily: fontFamilies.bold,
+    }
 });
 
 export default HomeScreen;
