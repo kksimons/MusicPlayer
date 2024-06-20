49cea5a3baa0eca24d4ce3f74ed70e762c5d19cb
kksimons <kksimons@gmail.com>
2024-06-20 15:57:28 -0600

    added data w/supabase

---
+++ src/data/songs.js
@@ -0,0 +1,39 @@
+export const recommendedSongs = [
+    [
+        {
+            url: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/Egzod__Maestro_Chives__Neoni_-_Royalty__NCS_Release_.mp3?t=2024-06-20T21%3A44%3A37.426Z',
+            title: 'Royalty',
+            artist: 'Egzod, Maestro Chives, Neoni',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/artwork-440x440.jpg?t=2024-06-20T21%3A48%3A53.736Z',
+        },
+        {
+            url: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/Cartoon__J_ja_-_On___On__feat._Daniel_Levi___NCS_Release_.mp3?t=2024-06-20T21%3A45%3A46.183Z',
+            title: 'On & On',
+            artist: 'Cartoon, Jéja, Daniel Levi',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/song2.jpg?t=2024-06-20T21%3A48%3A31.097Z',
+        },
+        {
+            url: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/Janji___Johnning_-_Nostalgia__NCS_Release_.mp3?t=2024-06-20T21%3A54%3A12.330Z',
+            title: 'Nostalgia',
+            artist: 'Johnning, Janji',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/notalgia.jpg?t=2024-06-20T21%3A52%3A25.749Z',
+        },
+        {
+            url: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/Aix_Cee_-_Feel_Like__NCS_Release_.mp3?t=2024-06-20T21%3A53%3A18.061Z',
+            title: 'Feel Like',
+            artist: 'Aix Cee',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/feellike.jpg?t=2024-06-20T21%3A53%3A55.347Z',
+        },
+        {
+            url: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/Warriyo_-_Mortals__feat._Laura_Brehm___NCS_Release_.mp3?t=2024-06-20T21%3A54%3A41.658Z',
+            title: 'Mortals (feat. Laura Brehm)',
+            artist: 'Warriyo, Laura Brehm',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://xzhjhmpuoilvmykxjses.supabase.co/storage/v1/object/public/Songs/marriko.jpg?t=2024-06-20T21%3A55%3A11.746Z',
+        },
+    ],
+]
---
+++ src/data/songsWithCategory.jsx
@@ -0,0 +1,16 @@
+import { recommendedSongs } from "./songs";
+
+export const songWithCategory = [
+    {
+        title: 'Recommended For You',
+        songs: recommendedSongs,
+    },
+    {
+        title: 'New Release',
+        songs: recommendedSongs,
+    },
+    {
+        title: 'NCS Songs',
+        songs: recommendedSongs,
+    },
+]
