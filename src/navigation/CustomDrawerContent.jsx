658a6118b8b64f4a502870f103e357204c3cfe6b
gigi-vu2804 <gigivu2804@gmail.com>
2024-07-24 13:33:13 -0600

    update navigation to display FollowScreen

--- src/navigation/CustomDrawerContent.jsx
+++ src/navigation/CustomDrawerContent.jsx
@@ -1,179 +1,221 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
-import { fontSize, iconSizes, spacing } from '../constants/dimensions';
-import { fontFamilies } from '../constants/fonts';
-import { useTheme } from '@react-navigation/native';
-import { useThemeStore } from '../store/themeStore';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {fontSize, iconSizes, spacing} from '../constants/dimensions';
+import {fontFamilies} from '../constants/fonts';
+import {useTheme} from '@react-navigation/native';
+import {useThemeStore} from '../store/themeStore';
 
 //icons
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Octicons from 'react-native-vector-icons/Octicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const CustomDrawerContent = (props) => {
-    const { colors } = useTheme();
-    const { isDarkMode, toggleTheme } = useThemeStore();
+const CustomDrawerContent = props => {
+  const {colors} = useTheme();
+  const {isDarkMode, toggleTheme} = useThemeStore();
 
-    const toggleDrawer = () => {
-        props.navigation.toggleDrawer();
-    };
+  const toggleDrawer = () => {
+    props.navigation.toggleDrawer();
+  };
 
-    return (
-        <DrawerContentScrollView style={[styles.container, { backgroundColor: colors.background }]}>
-            <View style={styles.headerIconContainer}>
-                <TouchableOpacity onPress={toggleDrawer}>
-                    <AntDesign name={"close"} color={colors.iconPrimary} size={iconSizes.lg} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => toggleTheme()}>
-                    <Octicons
-                        name={isDarkMode ? "sun" : "moon"}
-                        color={colors.iconPrimary}
-                        size={iconSizes.lg} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.drawerItemContainer}>
-                <DrawerItem
-                    label={"Home"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"home"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "HOME_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Liked Songs"}
-                    icon={() => (
-                        <AntDesign
-                            name={"hearto"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "LIKE_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Following"}
-                    icon={() => (
-                        <AntDesign
-                            name={"user"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "FOLLOW_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Player"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"music"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "PLAYER_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Playlists"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"list"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "PLAYLIST_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Contact Us"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"envelope-o"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "LIKE_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"FAQs"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"question-circle-o"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "LIKE_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Settings"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"cog"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "LIKE_SCREEN" }) }}
-                />
-                <DrawerItem
-                    label={"Sign Out"}
-                    icon={() => (
-                        <FontAwesome
-                            name={"cog"}
-                            size={iconSizes.md}
-                            color={colors.iconSecondary}
-                        />
-                    )}
-                    labelStyle={[styles.labelStyle, { color: colors.textPrimary }]}
-                    style={styles.drawerItem}
-                    onPress={() => { props.navigation.navigate("StackNavigation", { screen: "LOGIN_SCREEN" }) }}
-                />
-            </View>
-        </DrawerContentScrollView>
-    );
+  return (
+    <DrawerContentScrollView
+      style={[styles.container, {backgroundColor: colors.background}]}>
+      <View style={styles.headerIconContainer}>
+        <TouchableOpacity onPress={toggleDrawer}>
+          <AntDesign
+            name={'close'}
+            color={colors.iconPrimary}
+            size={iconSizes.lg}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => toggleTheme()}>
+          <Octicons
+            name={isDarkMode ? 'sun' : 'moon'}
+            color={colors.iconPrimary}
+            size={iconSizes.lg}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.drawerItemContainer}>
+        <DrawerItem
+          label={'Home'}
+          icon={() => (
+            <FontAwesome
+              name={'home'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'HOME_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Liked Songs'}
+          icon={() => (
+            <AntDesign
+              name={'hearto'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'LIKE_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Following'}
+          icon={() => (
+            <AntDesign
+              name={'user'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'FOLLOW_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Player'}
+          icon={() => (
+            <FontAwesome
+              name={'music'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'PLAYER_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Playlists'}
+          icon={() => (
+            <FontAwesome
+              name={'list'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'PLAYLIST_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Contact Us'}
+          icon={() => (
+            <FontAwesome
+              name={'envelope-o'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'LIKE_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'FAQs'}
+          icon={() => (
+            <FontAwesome
+              name={'question-circle-o'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'LIKE_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Settings'}
+          icon={() => (
+            <FontAwesome
+              name={'cog'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'LIKE_SCREEN',
+            });
+          }}
+        />
+        <DrawerItem
+          label={'Sign Out'}
+          icon={() => (
+            <FontAwesome
+              name={'cog'}
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={[styles.labelStyle, {color: colors.textPrimary}]}
+          style={styles.drawerItem}
+          onPress={() => {
+            props.navigation.navigate('StackNavigation', {
+              screen: 'LOGIN_SCREEN',
+            });
+          }}
+        />
+      </View>
+    </DrawerContentScrollView>
+  );
 };
 
 export default CustomDrawerContent;
 
 const styles = StyleSheet.create({
-    container: {
-        padding: spacing.lg,
-    },
-    headerIconContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    drawerItemContainer: {
-        marginVertical: spacing.lg
-    },
-    labelStyle: {
-        fontSize: fontSize.md,
-        fontFamily: fontFamilies.medium
-    },
-    drawerItem: {
-        marginVertical: spacing.sm,
-    }
+  container: {
+    padding: spacing.lg,
+  },
+  headerIconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  drawerItemContainer: {
+    marginVertical: spacing.lg,
+  },
+  labelStyle: {
+    fontSize: fontSize.md,
+    fontFamily: fontFamilies.medium,
+  },
+  drawerItem: {
+    marginVertical: spacing.sm,
+  },
 });
--- src/navigation/StackNavigation.jsx
+++ src/navigation/StackNavigation.jsx
@@ -1,27 +1,28 @@
 // src/navigation/StackNavigation.js
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import LikeScreen from '../screens/LikeScreen';
 import PlayerScreen from '../screens/PlayerScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
-import PlaylistScreen from '../screens/PlaylistScreen'; 
+import PlaylistScreen from '../screens/PlaylistScreen';
+import FollowScreen from '../screens/FollowScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigation = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="LOGIN_SCREEN" component={LoginScreen} />
-            <Stack.Screen name="SIGNUP_SCREEN" component={SignUpScreen} />
-            <Stack.Screen name="HOME_SCREEN" component={HomeScreen} />
-            <Stack.Screen name="LIKE_SCREEN" component={LikeScreen} />
-            <Stack.Screen name="PLAYER_SCREEN" component={PlayerScreen} />
-            <Stack.Screen name="PLAYLIST_SCREEN" component={PlaylistScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="LOGIN_SCREEN" component={LoginScreen} />
+      <Stack.Screen name="SIGNUP_SCREEN" component={SignUpScreen} />
+      <Stack.Screen name="HOME_SCREEN" component={HomeScreen} />
+      <Stack.Screen name="LIKE_SCREEN" component={LikeScreen} />
+      <Stack.Screen name="PLAYER_SCREEN" component={PlayerScreen} />
+      <Stack.Screen name="PLAYLIST_SCREEN" component={PlaylistScreen} />
+      <Stack.Screen name="FOLLOW_SCREEN" component={FollowScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default StackNavigation;
